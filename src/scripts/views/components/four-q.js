2a4a580473eba50c4e218f68cffbc764b3922e1b
Alif Fathir Rachman <leafalifeather@gmail.com>
2022-12-11 00:28:44 +0700

    Add warning system for four-q

--- src/scripts/views/components/four-q.js
+++ src/scripts/views/components/four-q.js
@@ -16,39 +16,47 @@ class FourQ extends HTMLElement {
       <button id="add_task">Add Task</button>
     </div>
     <div class="quadrant" id="do">
-    <button id="info-do" class="quadrant-info">
-      <i class="fa fa-info-circle" aria-hidden="true"></i>
-    </button>
-      <div class='four-q-grid-title'> 
-        <h3>Important and Urgent</h3>
-        <p>Do it Now !</p>
+      <div class='four-q-grid-title'>
+        <div> 
+          <h3>Important and Urgent</h3>
+          <p>Do it Now !</p>
+        </div>
+        <button id="info-do" class="quadrant-info">
+          <i class="fa fa-info-circle" aria-hidden="true"></i>
+        </button>
       </div>
     </div>
     <div class="quadrant" id="schedule">
-    <button id="info-schedule" class="quadrant-info">
-      <i class="fa fa-info-circle" aria-hidden="true"></i>
-    </button>
-      <div class='four-q-grid-title'> 
-        <h3>Important</h3>
-        <p>Scheduled it !</p>
+      <div class='four-q-grid-title'>
+        <div>
+          <h3>Important</h3>
+          <p>Scheduled it !</p>
+        </div>
+        <button id="info-schedule" class="quadrant-info">
+          <i class="fa fa-info-circle" aria-hidden="true"></i>
+        </button>
       </div>
     </div>
     <div class="quadrant" id="delegate">
-      <button id="info-delegate" class="quadrant-info">
-        <i class="fa fa-info-circle" aria-hidden="true"></i>
-      </button>
-      <div class='four-q-grid-title'> 
-        <h3>Urgent</h3>
-        <p>Delegate it !</p>
+      <div class='four-q-grid-title'>
+        <div> 
+          <h3>Urgent</h3>
+          <p>Delegate it !</p>
+        </div>
+        <button id="info-delegate" class="quadrant-info">
+          <i class="fa fa-info-circle" aria-hidden="true"></i>
+        </button>
       </div>
     </div>
     <div class="quadrant" id="eliminate">
-      <button id="info-eliminate" class="quadrant-info">
-        <i class="fa fa-info-circle" aria-hidden="true"></i>
-      </button>
-      <div class='four-q-grid-title'> 
-        <h3>Not Important and Not Urgent</h3>
-        <p>Eliminate it if possible !</p>
+      <div class='four-q-grid-title'>
+        <div> 
+          <h3>Not Important Nor Urgent</h3>
+          <p>Eliminate it !</p>
+        </div>
+        <button id="info-eliminate" class="quadrant-info">
+          <i class="fa fa-info-circle" aria-hidden="true"></i>
+        </button>
       </div>
     </div>
     `;
@@ -74,6 +82,9 @@ class FourQ extends HTMLElement {
         doElement.appendChild(taskItemElement);
       } else if (this._tasks[task].importance) {
         scheduleElement.appendChild(taskItemElement);
+        if (!this._tasks[task].schedule) {
+          scheduleElement.classList.add('warning');
+        }
       } else if (this._tasks[task].urgency) {
         delegateElement.appendChild(taskItemElement);
       } else {
@@ -81,7 +92,7 @@ class FourQ extends HTMLElement {
       }
     });
 
-    const doChildElement = doElement.childElementCount - 2;
+    const doChildElement = doElement.childElementCount - 1;
     if (doChildElement > 2) {
       doElement.classList.add('warning');
     }
@@ -91,29 +102,29 @@ class FourQ extends HTMLElement {
         switch (element.getAttribute('id')) {
           case 'info-do':
             Swal.fire(
-              'Kuadran 1',
-              'Kuadran satu dipenuhi dengan aktivitas yang sangat mendesak dan penting atau aktivitas yang harus diselesaikan pada waktu yang sangat dekat. Bila aktivitas pada kuadran I tidak dikerjakan sesegera mungkin, akan mendatangkan masalah di kemudian hari.',
-
+              'Important and Urgent',
+              'Kuadran ini dipenuhi dengan aktivitas yang sangat mendesak dan penting atau aktivitas yang harus diselesaikan pada waktu yang sangat dekat. Bila aktivitas pada kuadran ini tidak dikerjakan sesegera mungkin, akan mendatangkan masalah di kemudian hari.',
+              'info',
             );
             break;
           case 'info-delegate':
             Swal.fire(
-              'Kuadran 2',
-              'Kuadran dua diisi dengan aktivitas-aktivitas yang penting namun tidak mendesak dan masih memiliki waktu yang banyak untuk dikerjakan. Fokus dalam kuadran dua menekankan pada pencapaian tujuan panjang.',
+              'Urgent',
+              'Kuadran ini diisi dengan aktivitas-aktivitas yang penting namun tidak mendesak dan masih memiliki waktu yang banyak untuk dikerjakan. Fokus dalam kuadran ini menekankan pada pencapaian tujuan panjang.',
               'info',
             );
             break;
           case 'info-schedule':
             Swal.fire(
-              'Kuadran 3',
-              'Aktivitas pada kuadran tidak terlalu penting untuk diselesaikan dengan cepat. Berbeda dengan kuadran satu, kegiatan di kuadran tiga dapat ditunda sampai aktivitas di kuadran pertama selesai dikerjakan.',
+              'Important',
+              'Aktivitas pada kuadran ini tidak terlalu penting untuk diselesaikan dengan cepat. Berbeda dengan kuadran Important and Urgent, kegiatan di kuadran ini dapat ditunda sampai aktivitas di kuadran pertama selesai dikerjakan.',
               'info',
             );
             break;
           case 'info-eliminate':
             Swal.fire(
-              'Kuadran 4',
-              'Kuadran empat berisi aktivitas tidak mendesak dan tidak penting atau bisa juga disebut kegiatan yang membawa kebahagiaan sesaat. Oleh karena itu, yang termasuk dalam kuadran empat setidaknya perlu dikurangi serta diminimalisir agar tidak membuang-buang waktu.',
+              'Not Important Nor Urgent',
+              'Kuadran ini berisi aktivitas tidak mendesak dan tidak penting atau bisa juga disebut kegiatan yang membawa kebahagiaan sesaat. Oleh karena itu, yang termasuk dalam kuadran empat setidaknya perlu dikurangi serta diminimalisir agar tidak membuang-buang waktu.',
               'info',
             );
             break;
