3f30ded4e8425266c554bbffc53e02a3be5c125b
Alif Fathir Rachman <leafalifeather@gmail.com>
2022-12-11 19:10:02 +0700

    Deleted calendar and Task list

--- src/scripts/views/components/content-tab.js
+++ src/scripts/views/components/content-tab.js
@@ -14,14 +14,10 @@ class ContentTab extends HTMLElement {
     this.innerHTML = /* HTML */`
     <tab-group>
       <tab-title-list>
-          <tab-title><button>Calendar</button></tab-title>
           <tab-title active><button>Four Quadrant</button></tab-title>
-          <tab-title><button>Task List</button></tab-title>
       </tab-title-list>
       <tab-content-list>
-          <tab-content><calendar-element></calendar-element></tab-content>
           <tab-content><four-q></four-q></tab-content>
-          <tab-content><task-list></task-list></tab-content>
       </tab-content-list>
     </tab-group>
     `;
